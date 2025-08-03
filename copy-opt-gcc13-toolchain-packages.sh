06100a3ed51c6c73a7cd60973bb04e03f213807d
Tim Stahlhut <stahta01@gmail.com>
2025-08-03 13:28:26 -0400

    script: Update copy-opt-gcc13-toolchain-packages.sh

--- copy-opt-gcc13-toolchain-packages.sh
+++ copy-opt-gcc13-toolchain-packages.sh
@@ -1,15 +1,17 @@
 mkdir -p packages/opt_gcc13_toolchain_MSYS_64bit_packages
 
 rm -f ./packages/opt_gcc13_toolchain_MSYS_64bit_packages/*.zst
-cp --preserve=timestamps -f ./opt-zlib/opt-zlib-1.3.1-1-x86_64.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_64bit_packages/
-cp --preserve=timestamps -f ./opt-binutils/opt-binutils-2.44-1-x86_64.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_64bit_packages/
-cp --preserve=timestamps -f ./opt-gcc13/opt-gcc-13.4.1+d20250702.r0.c0.g04a71a35c3d0-1-x86_64.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_64bit_packages/
-cp --preserve=timestamps -f ./opt-gcc13/opt-gcc-libs-13.4.1+d20250702.r0.c0.g04a71a35c3d0-1-x86_64.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./opt-zlib/opt-zlib-1.3.1-5-x86_64.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./opt-gcc13/opt-gcc13-13.4.1+d20250731.r3.c0.g1afe5101b179-2-x86_64.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./opt-gcc13/opt-gcc13-libs-13.4.1+d20250731.r3.c0.g1afe5101b179-2-x86_64.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./opt-libtool-gcc13/opt-libltdl-2.5.4-8-x86_64.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./opt-libtool-gcc13/opt-libtool-2.5.4-8-x86_64.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_64bit_packages/
 
 mkdir -p packages/opt_gcc13_toolchain_MSYS_32bit_packages
 
 rm -f ./packages/opt_gcc13_toolchain_MSYS_32bit_packages/*.zst
-cp --preserve=timestamps -f ./opt-zlib/opt-zlib-1.3.1-1-i686.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_32bit_packages/
-cp --preserve=timestamps -f ./opt-binutils/opt-binutils-2.44-1-i686.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_32bit_packages/
-cp --preserve=timestamps -f ./opt-gcc13/opt-gcc-13.4.1+d20250702.r0.c0.g04a71a35c3d0-1-i686.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_32bit_packages/
-cp --preserve=timestamps -f ./opt-gcc13/opt-gcc-libs-13.4.1+d20250702.r0.c0.g04a71a35c3d0-1-i686.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./opt-zlib/opt-zlib-1.3.1-5-i686.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./opt-gcc13/opt-gcc13-13.4.1+d20250731.r3.c0.g1afe5101b179-2-i686.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./opt-gcc13/opt-gcc13-libs-13.4.1+d20250731.r3.c0.g1afe5101b179-2-i686.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./opt-libtool-gcc13/opt-libltdl-2.5.4-8-i686.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./opt-libtool-gcc13/opt-libtool-2.5.4-8-i686.pkg.tar.zst ./packages/opt_gcc13_toolchain_MSYS_32bit_packages/
